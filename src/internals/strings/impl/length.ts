6ef16660cc48277b49dc4853c22e78431f128a51
ecyrbe <ecyrbe@gmail.com>
2023-02-17 05:06:52 +0100

    refactor(string): simplify a bit more

--- src/internals/strings/impl/length.ts
+++ src/internals/strings/impl/length.ts
@@ -24,7 +24,7 @@ type LengthUp<
     ? StrIter.Size<It> extends 12 // 2^13 is the last block size within the complexity limit
       ? LengthDown<$Rest, Add<$Length, StrIter.Value<$DoubleIt>>, $DoubleIt>
       : LengthUp<$Rest, Add<$Length, StrIter.Value<$DoubleIt>>, $DoubleIt>
-    : StrIter.CutAt<Str, It> extends infer $Rest extends string
+    : StrIter.CutAt<Str, It> extends `${infer $Rest}`
     ? LengthUp<$Rest, Add<$Length, StrIter.Value<It>>, It>
     : LengthDown<Str, $Length, StrIter.Prev<It>>
   : never;
@@ -34,7 +34,7 @@ type LengthDown<
   $Length extends number | bigint,
   It
 > = It extends StrIter.Iterator
-  ? StrIter.CutAt<Str, It> extends infer $Rest extends string
+  ? StrIter.CutAt<Str, It> extends `${infer $Rest}`
     ? LengthDown<$Rest, Add<$Length, StrIter.Value<It>>, It>
     : LengthDown<Str, $Length, StrIter.Prev<It>>
   : $Length;
