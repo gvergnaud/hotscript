734ae26703aaa4c2974f70d86aa0ff5b0979f001
ecyrbe <ecyrbe@gmail.com>
2023-02-17 10:46:01 +0100

    docs(strings): improve length algorithm description

--- src/internals/strings/impl/length.ts
+++ src/internals/strings/impl/length.ts
@@ -5,38 +5,55 @@ import { StringIterator as StrIter } from "./utils";
 
 // Implementation of the following algorithm:
 //
-// function length(s, acc) {
-//   if (s === "") {
-//       return 0;
-//   } else if (s.indexOf(acc) === 0) {
-//       return acc.length + length(s.slice(acc.length), acc + acc);
-//   } else {
-//       return length(s, acc.slice(acc.length / 2));
+// function lengthUp(s, length = 0, it = It.init()) {
+//   const doubleIt = It.double(it);
+//   let rest = s.slice(It.size(doubleIt));
+//   if (rest) {
+//     if (It.size(it) === 12) {
+//       return lengthDown(rest, length + It.value(doubleIt), doubleIt);
+//     }
+//     return lengthUp(rest, length + It.value(doubleIt), doubleIt);
 //   }
+//   rest = s.slice(It.size(it));
+//   if (rest) {
+//     return lengthUp(rest, length + It.value(it), it);
+//   }
+//   return lengthDown(s, length, It.prev(it));
+// }
+//
+// function lengthDown(s, length, it) {
+//  if(it) {
+//    const rest = s.slice(It.size(it));
+//    if (rest) {
+//      return lengthDown(rest, length + It.value(it), it);
+//    }
+//    return lengthDown(s, length, It.prev(it));
+//  }
+//  return length;
 // }
 
 type LengthUp<
   Str extends string,
-  $Length extends number | bigint = 0,
+  Length extends number | bigint = 0,
   It extends StrIter.Iterator = StrIter.Init
 > = StrIter.Double<It> extends infer $DoubleIt extends StrIter.Iterator
   ? StrIter.CutAt<Str, $DoubleIt> extends `${infer $Rest}`
     ? StrIter.Size<It> extends 12 // 2^13 is the last block size within the complexity limit
-      ? LengthDown<$Rest, Add<$Length, StrIter.Value<$DoubleIt>>, $DoubleIt>
-      : LengthUp<$Rest, Add<$Length, StrIter.Value<$DoubleIt>>, $DoubleIt>
+      ? LengthDown<$Rest, Add<Length, StrIter.Value<$DoubleIt>>, $DoubleIt>
+      : LengthUp<$Rest, Add<Length, StrIter.Value<$DoubleIt>>, $DoubleIt>
     : StrIter.CutAt<Str, It> extends `${infer $Rest}`
-    ? LengthUp<$Rest, Add<$Length, StrIter.Value<It>>, It>
-    : LengthDown<Str, $Length, StrIter.Prev<It>>
+    ? LengthUp<$Rest, Add<Length, StrIter.Value<It>>, It>
+    : LengthDown<Str, Length, StrIter.Prev<It>>
   : never;
 
 type LengthDown<
   Str extends string,
-  $Length extends number | bigint,
+  Length extends number | bigint,
   It
 > = It extends StrIter.Iterator
   ? StrIter.CutAt<Str, It> extends `${infer $Rest}`
-    ? LengthDown<$Rest, Add<$Length, StrIter.Value<It>>, It>
-    : LengthDown<Str, $Length, StrIter.Prev<It>>
-  : $Length;
+    ? LengthDown<$Rest, Add<Length, StrIter.Value<It>>, It>
+    : LengthDown<Str, Length, StrIter.Prev<It>>
+  : Length;
 
 export type Length<T extends string> = T extends "" ? 0 : LengthUp<T>;
