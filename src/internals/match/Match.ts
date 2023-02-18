917baaf3ef24bce8d11367f406d7be843e55d63f
gvergnaud <gabriel.vergnaud@gmail.com>
2023-02-18 16:35:08 +0100

    feat(match): also expose Match.arg

--- src/internals/match/Match.ts
+++ src/internals/match/Match.ts
@@ -41,4 +41,8 @@ export namespace Match {
   export type arg1<Constraint = unknown> = Impl.arg<1, Constraint>;
   export type arg2<Constraint = unknown> = Impl.arg<2, Constraint>;
   export type arg3<Constraint = unknown> = Impl.arg<3, Constraint>;
+  export type arg<Index extends number, Constraint = unknown> = Impl.arg<
+    Index,
+    Constraint
+  >;
 }
